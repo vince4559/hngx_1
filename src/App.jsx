f4f5fe82208fbcfe8cbc3f6c3ad51e9ff987cdd4
aigbojevincent6@gmail.com <aigbojevincent6@gmail.com>
2023-09-08 12:15:04 +0100

    prepering project

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import hng from '../public/hng.jpg';
+
+
+export default function App() {
+  let date = new Date();
+    date.getDay();
+  
+  const currentDayOfTheWeek = new Intl.DateTimeFormat(["en"], {
+    weekday: "long" 
+}).format(date);
+
+const currentUTCTime = date.toUTCString().slice(16);  
+  return (
+    <main>
+        <h1 id="slackUserName" ><span>Slack Name: </span> vincent6</h1>
+        <img 
+          id="slackDisplayImage"
+          alt="slackUserName"            
+          src={hng}
+          className='w-40 h-50 rounded-xl'
+        />
+        <p id="currentDayOfTheWeek"><span>Current Day Of the Week: </span> 
+          {currentDayOfTheWeek}
+        </p>
+
+        <p id="currentUTCTime"><span>Current UTC time: </span> {currentUTCTime} </p>
+
+        <p id="myTrack"><span>Track: </span> FrontEnd</p>
+
+        <a id="githubURL" href="" target="_blank">Github URL</a>
+      </main>
+  )
+}
